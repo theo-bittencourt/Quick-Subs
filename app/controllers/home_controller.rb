feaaa6f5e9f3335b07d866ed262c36a879b67ada
Theo Bittencourt <theodrums@gmail.com>
2011-06-02 16:23:29 -0300

    fix -- notice hack for heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if params[:q].present?
       @subtitles = Subtitle.search params[:q]
-
+      flash[:notice] = nil
       flash[:notice] = "Nenhuma legenda encontrada" if @subtitles.nil?
     end
   end
